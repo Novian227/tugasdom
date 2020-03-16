e3b5fe61debf28c6504c02d1fb02f624af6a07df
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-03-16 19:19:58 +0700

    Update tugas.js

--- tugas.js
+++ tugas.js
@@ -1,7 +1,8 @@
+var phi = 3.14;
 var sambit = document.querySelector("button"); 
 
 
-document.getElementById('opsiPerhitungan').addEventListener('change', function () {
+document.getElementsByName('opsiPerhitungan')[0].addEventListener('change', function () {
     document.getElementById('judul_hitung').innerHTML = this.value 
     document.getElementById('hasil_perhitungan').innerHTML = 0   
     var radius = document.getElementsByName('jari')[0].value = 0;
@@ -14,22 +15,22 @@ sambit.addEventListener('click', function () {
     var title = document.getElementById('judul_hitung').innerHTML;
     
     if (title == 'Luas Bola') {											// RUMUS LUAS BOLA
-        let result = 4 * Math.PI * Math.pow(radius, 2)
+        let result = 4 * phi * Math.pow(radius, 2)
         document.getElementById('hasil_perhitungan').innerHTML = result
     }
 
     if (title == 'Volume Bola') {										// RUMUS VOLUME BOLA
-        let result = (4/3) * Math.PI * Math.pow(radius, 3)
+        let result = (4/3) * phi * Math.pow(radius, 3)
         document.getElementById('hasil_perhitungan').innerHTML = result
     }
 
     if (title == 'Luas Tabung') {										// RUMUS LUAS TABUNG
-        let result = 2 * Math.PI * radius * (height + radius)
+        let result = 2 * phi * radius * (height + radius)
         document.getElementById('hasil_perhitungan').innerHTML = result
     }
 
     if (title == 'Volume Tabung') {										// RUMUS VOLUME TABUNG
-        let result = Math.PI * Math.pow(radius, 2) * height
+        let result = phi * Math.pow(radius, 2) * height
         document.getElementById('hasil_perhitungan').innerHTML = result
     }
 })
